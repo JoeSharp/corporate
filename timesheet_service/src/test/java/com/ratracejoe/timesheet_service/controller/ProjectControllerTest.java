f3227ca87262e7f352c4db8c6ac7516268059f30
Joe Sharp <ratracejoe@hotmail.com>
2023-10-04 18:30:54 +0100

    pulling out stuff that will be common to other controllers

--- timesheet_service/src/test/java/com/ratracejoe/timesheet_service/controller/ProjectControllerTest.java
+++ timesheet_service/src/test/java/com/ratracejoe/timesheet_service/controller/ProjectControllerTest.java
@@ -1,48 +1,19 @@
 package com.ratracejoe.timesheet_service.controller;
 
 import com.ratracejoe.timesheet_service.entity.Project;
-import com.ratracejoe.timesheet_service.util.ProjectUtils;
-import dasniko.testcontainers.keycloak.KeycloakContainer;
+import com.ratracejoe.timesheet_service.util.AbstractTest;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.json.JacksonJsonParser;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.*;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
 
-import java.net.URI;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-class ProjectControllerTest {
 
-    static KeycloakContainer keycloak;
-    static {
-        keycloak = new KeycloakContainer().withRealmImportFile("keycloak/realm-export.json");
-        keycloak.start();
-    }
-
-    @DynamicPropertySource
-    static void registerResourceServerIssuerProperty(DynamicPropertyRegistry registry) {
-        registry.add(
-                "spring.security.oauth2.resourceserver.jwt.issuer-uri",
-                () -> keycloak.getAuthServerUrl() + "/realms/ratracejoe");
-    }
-
-    @Autowired
-    private TestRestTemplate restTemplate;
-
-    @Autowired
-    private ProjectUtils utils;
+class ProjectControllerTest extends AbstractTest {
 
     @BeforeEach
     public void beforeEach() {
@@ -54,37 +25,12 @@ class ProjectControllerTest {
         utils.clearProjects();
     }
 
-    private HttpHeaders authorizedHeaders() {
-        URI authorizationURI = URI.create(keycloak.getAuthServerUrl() + "/realms/ratracejoe/protocol/openid-connect/token");
-        MultiValueMap<String, String> formData = new LinkedMultiValueMap<>();
-        formData.add("grant_type", "password");
-        formData.add("client_id", "timesheets-service");
-        formData.add("username", "joe.sharp@ratracejoe.co.uk");
-        formData.add("password", "s3cr3t");
-        formData.add("client_secret", "rX0uyWb89PxdeclkQoLMtmRtCLRxFlKy");
-
-        HttpHeaders loginHeaders = new HttpHeaders();
-        loginHeaders.set(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_FORM_URLENCODED_VALUE);
-        var result = restTemplate.exchange(authorizationURI,
-                HttpMethod.POST,
-                new HttpEntity<>(formData, loginHeaders),
-                String.class);
-
-        JacksonJsonParser jsonParser = new JacksonJsonParser();
-        String authToken = "Bearer " + jsonParser.parseMap(result.getBody())
-                .get("access_token")
-                .toString();
-
-        var headers = new HttpHeaders();
-        headers.set(HttpHeaders.AUTHORIZATION, authToken);
-        return headers;
-    }
 
     @Test
     public void greetingShouldReturnDefaultMessage() throws Exception {
         var response = restTemplate.exchange("/project/hello/Joe",
                 HttpMethod.GET,
-                new HttpEntity<>(null, authorizedHeaders()),
+                new HttpEntity<>(null, authorizedHeaders(restTemplate)),
                 String.class);
 
         assertEquals(HttpStatus.OK, response.getStatusCode());
@@ -97,7 +43,7 @@ class ProjectControllerTest {
         var response =
                 restTemplate.exchange("/project/",
                 HttpMethod.GET,
-                new HttpEntity<>(null, authorizedHeaders()),
+                new HttpEntity<>(null, authorizedHeaders(restTemplate)),
                 new ParameterizedTypeReference<List<Project>>() {
                 });
 
---
+++ timesheet_service/src/test/java/com/ratracejoe/timesheet_service/util/AbstractTest.java
@@ -0,0 +1,65 @@
+package com.ratracejoe.timesheet_service.util;
+
+import dasniko.testcontainers.keycloak.KeycloakContainer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.json.JacksonJsonParser;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+
+import java.net.URI;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class AbstractTest {
+    @Autowired
+    protected TestRestTemplate restTemplate;
+
+    @Autowired
+    protected ProjectUtils utils;
+
+    @DynamicPropertySource
+    static void registerResourceServerIssuerProperty(DynamicPropertyRegistry registry) {
+        registry.add(
+                "spring.security.oauth2.resourceserver.jwt.issuer-uri",
+                () -> keycloak.getAuthServerUrl() + "/realms/ratracejoe");
+    }
+
+    static KeycloakContainer keycloak;
+    static {
+        keycloak = new KeycloakContainer().withRealmImportFile("keycloak/realm-export.json");
+        keycloak.start();
+    }
+
+    protected static HttpHeaders authorizedHeaders(TestRestTemplate restTemplate) {
+        URI authorizationURI = URI.create(keycloak.getAuthServerUrl() + "/realms/ratracejoe/protocol/openid-connect/token");
+        MultiValueMap<String, String> formData = new LinkedMultiValueMap<>();
+        formData.add("grant_type", "password");
+        formData.add("client_id", "timesheets-service");
+        formData.add("username", "joe.sharp@ratracejoe.co.uk");
+        formData.add("password", "s3cr3t");
+        formData.add("client_secret", "rX0uyWb89PxdeclkQoLMtmRtCLRxFlKy");
+
+        HttpHeaders loginHeaders = new HttpHeaders();
+        loginHeaders.set(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_FORM_URLENCODED_VALUE);
+        var result = restTemplate.exchange(authorizationURI,
+                HttpMethod.POST,
+                new HttpEntity<>(formData, loginHeaders),
+                String.class);
+
+        JacksonJsonParser jsonParser = new JacksonJsonParser();
+        String authToken = "Bearer " + jsonParser.parseMap(result.getBody())
+                .get("access_token")
+                .toString();
+
+        var headers = new HttpHeaders();
+        headers.set(HttpHeaders.AUTHORIZATION, authToken);
+        return headers;
+    }
+}
